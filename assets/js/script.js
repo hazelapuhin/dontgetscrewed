2d5feeb9eed54700d16edb983b0abc3787fe27ce
unknown <hazel.apuhin@gmail.com>
2018-08-02 16:46:03 +0800

    includes game activity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,40 @@
+$(function (){
+	//
 
 
-$('button').click( ( ) => {
-    let mass = $('#mass').val();
-    let height = $('height').val();
-    const bmi = mass / Math.pow(height,2)
-    if (bmi<18.5) {
-    $('#bmi').html(bmi + ':underweight');
-    } else if (bmi<25) {
-    $('#bmi').html(bmi + ':healthy weight');
-    }else if (bmi<30) {
-    $('#bmi').html(bmi + ':above ideal range');
-    }else {
-    $('#bmi').html(bmi + ':obese');
-}
-)}
+	var container = $('#container')
+	var fairy = $('#fairy')
+	var obstacle = $('.obstacle')
+	var obstacle1 = $('#obstacle1')
+	var obstacle2 = $('#obstacle2')
+	var	score = $('#score')
+	var speed = $('#speed')
+	var	restart	= $('#restart')
+
+	var contwidth = parseInt(container.width());
+	var contheight = parseInt(container.height())
+	var obsposition = parseInt(obstacle.css('right'));
+	var obsheight = parseInt(obstacle.css('height'));
+	var fairy_left = parseInt(fairy.css('left'));
+	var fairy_height = parseInt(fairy.height());
+	var speed = 50;
+
+	var the_training = setInterval(function () {
+
+		var obscurrent_position = parseInt(obstacle.css('right'));
+
+		if (obscurrent_position > contwidth) {
+			var new_height = parseInt(Math.random()*100);
+		}
+
+			
+		//mov the obstacle to the right
+		obstacle.css('right',obscurrent_position + speed);
+
+
+
+
+
+	}, 40);
+
+})
